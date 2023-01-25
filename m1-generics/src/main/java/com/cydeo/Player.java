bd18c9585f39efd5e16f10cd97761ff2c3af3bde
alish_codex <alisher_nizamov@hotmail.com>
2023-01-24 20:14:52 -0500

    Created more classes

---
+++ m1-generics/src/main/java/com/cydeo/Player.java
@@ -0,0 +1,16 @@
+package com.cydeo;
+
+public abstract class Player { //we will not create objects
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+}
