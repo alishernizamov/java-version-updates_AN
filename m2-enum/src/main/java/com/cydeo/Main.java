1eaa4113a0994e111bbb10956baab097424b43ed
alish_codex <alisher_nizamov@hotmail.com>
2023-01-24 22:21:05 -0500

    updates

---
+++ m2-enum/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,67 @@
+package com.cydeo;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("How to access a constant");
+
+        Currency c = Currency.DIME;
+        System.out.println(c); //DIME
+
+        System.out.println(Currency.PENNY);
+
+        System.out.println("How to access all constants");
+
+        Currency[] currencies = Currency.values(); //
+
+        for(Currency currency : currencies){
+            System.out.println(currency);
+        }
+
+        System.out.println("How to use switch case");
+
+        switch (Currency.PENNY){
+            case PENNY:
+                System.out.println("It is 1 cent");
+                break;
+            case NICKLE:
+                System.out.println("It is 5 cents");
+                break;
+            case DIME:
+                System.out.println("It is 10 cents");
+                break;
+            case QUARTER:
+                System.out.println("It is 25 cents");
+                break;
+        }
+
+        System.out.println("Operation task");
+        calculate(3, 5, Operation.PLUS);
+
+    }
+
+    private static void calculate(double x, double y, Operation ooperation){
+
+        switch (ooperation){
+            case PLUS:
+                System.out.println(x+y);
+                break;
+            case MINUS:
+                System.out.println(x-y);
+                break;
+            case MULTIPLE:
+                System.out.println(x*y);
+                break;
+            case DIVIDE:
+                System.out.println(x/y);
+                break;
+
+
+        }
+
+
+    }
+
+}
---
+++ m2-enum/src/main/java/com/cydeo/Operation.java
@@ -0,0 +1,9 @@
+package com.cydeo;
+
+public enum Operation {
+
+    PLUS, MINUS, MULTIPLE, DIVIDE; //each constant is object(new) of Operation
+
+    //how to assign value to each enum object
+
+}
